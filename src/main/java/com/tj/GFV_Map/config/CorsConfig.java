990f89ef99b023fd09a431e3d5f2486e2a94fed3
ksw1360 <ksw1360@gmail.com>
2026-06-11 15:03:27 +0900

    오류 수정
    오류 수정

--- src/main/java/com/tj/GFV_Map/config/CorsConfig.java
+++ src/main/java/com/tj/GFV_Map/config/CorsConfig.java
@@ -3,22 +3,18 @@ package com.tj.GFV_Map.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://master.d2ahqjf7y3gh6b.amplifyapp.com/",
-                        "http://192.168.7.120:3000/auth/google/login",
-                        "https://master.d2ahqjf7y3gh6b.amplifyapp.com/",
-                        "https://hjp7208.site",
-                        "http://hjp7208.site"
-// 1. AWS Elastic Beanstalk
-// 2. 가비아 연동 주소 추가 예정
-//                        "https://main.d1t17ie3uipfsv.amplifyapp.com"
-//                        "https://www.ksw1360.asia",    // ← 추가!! (이게 진짜 핵심)
-//                        "https://ksw1360.asia"          // ← www 없는 버전도 추가
+                        "https://master.d2ahqjf7y3gh6b.amplifyapp.com", // Amplify (슬래시 X)
+                        "https://hjp7208.site",                         // 가비아
+                        "http://hjp7208.site",
+                        "http://localhost:3000",                        // 로컬 개발
+                        "http://192.168.7.120:3000"                     // LAN 테스트 (경로 X)
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
